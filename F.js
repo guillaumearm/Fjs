a77fd2552642b4bc5be1083fff07611039928d79
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-02-03 16:56:27 +0100

    expose F only

--- F.js
+++ F.js
@@ -5,16 +5,16 @@
 
 // TODO: apply function
 
-export const curry = (f, ...xs) =>
+const curry = (f, ...xs) =>
 	xs.length === f.length
 		? f(...xs)
 		: curry.bind(this, f, ...xs)
 		
-export const merge = obj => (...changes) => Object.assign({}, obj, ...changes)
+const merge = obj => (...changes) => Object.assign({}, obj, ...changes)
 
-export const compose = f => g => x => f(g(x))
-export const foldl = f => acc => xs => xs.reduce(f, acc)
-export const foldr = f => acc => xs => xs.reverse().reduce(f, acc)
+const compose = f => g => x => f(g(x))
+const foldl = f => acc => xs => xs.reduce(f, acc)
+const foldr = f => acc => xs => xs.reverse().reduce(f, acc)
 
 export default {curry, merge, compose, foldl, foldr}
 
