aaeb9933304cdacf74db26103ce97eb5c601d8ed
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-02-03 17:07:24 +0100

    re-add export

--- F.js
+++ F.js
@@ -5,16 +5,16 @@
 
 // TODO: apply function
 
-const curry = (f, ...xs) =>
+export const curry = (f, ...xs) =>
 	xs.length === f.length
 		? f(...xs)
 		: curry.bind(this, f, ...xs)
 		
-const merge = obj => (...changes) => Object.assign({}, obj, ...changes)
+export const merge = obj => (...changes) => Object.assign({}, obj, ...changes)
 
-const compose = f => g => x => f(g(x))
-const foldl = f => acc => xs => xs.reduce(f, acc)
-const foldr = f => acc => xs => xs.reverse().reduce(f, acc)
+export const compose = f => g => x => f(g(x))
+export const foldl = f => acc => xs => xs.reduce(f, acc)
+export const foldr = f => acc => xs => xs.reverse().reduce(f, acc)
 
 export default {curry, merge, compose, foldl, foldr}
 
